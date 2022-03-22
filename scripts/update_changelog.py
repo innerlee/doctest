ce6fc14ceb0d787dc882d829460f6d67a1607acc
Sourcery AI <>
2022-03-22 00:11:01 +0000

    'Refactored by Sourcery'

--- scripts/update_changelog.py
+++ scripts/update_changelog.py
@@ -8,6 +8,6 @@ with open("version.txt") as f: version = f.read()
 # update changelog
 print("updating changelog")
 os.chdir("../")
-os.system("github_changelog_generator --future-release " + version)
+os.system(f"github_changelog_generator --future-release {version}")
 os.system("git add CHANGELOG.md")
 os.chdir("scripts")
